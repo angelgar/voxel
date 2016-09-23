372518868f22b30434b936161696c4856a8ede49
muschellij2 <muschellij2@gmail.com>
2016-09-23 14:06:45 -0400

    Updated some docs and some namespace.

--- R/plotGAM.R
+++ R/plotGAM.R
@@ -1,74 +1,71 @@
 #' GAM plotting using ggplot2
-#'  
+#'
 #' @family Plotting
 #' @param gamFit fitted gam model as produced by mgcv::gam()
 #' @param smooth.cov (character) name of smooth term to be plotted
 #' @param groupCovs (character)  name of group variable to plot by, if NULL (default) then there are no groups in plot
 #' @param orderedAsFactor if TRUE (default) then the model is refitted with ordered variables as factors.
-#' @param rawOrFitted If FALSE (default) then only smooth terms are plotted; if rawOrFitted = "raw" then raw values are plotted against smooth; if rawOrFitted = "fitted" then fitted values are plotted against smooth 
+#' @param rawOrFitted If FALSE (default) then only smooth terms are plotted; if rawOrFitted = "raw" then raw values are plotted against smooth; if rawOrFitted = "fitted" then fitted values are plotted against smooth
 #' @param plotCI if TRUE (default) upper and lower confidence intervals are added at 2 standard errors above and below the mean
-#' 
+#'
 #' @return Returns a ggplot object that can be visualized using the print() function
-#' 
+#'
 #' @export
 #' @examples
-#' 
+#'
 #' data <- data.frame(x = rep(1:20, 2), group = rep(1:2, each = 20))
 #' set.seed(1)
-#' data$y <- (data$x^2)*data$group*3 + rnorm(40, sd = 200) 
+#' data$y <- (data$x^2)*data$group*3 + rnorm(40, sd = 200)
 #' data$group <- ordered(data$group)
-#' 
+#'
 #' gam <- mgcv::gam(y ~ s(x) + group, data=data)
-#' 
-#' plot1 <- plotGAM(gamFit = gam, smooth.cov = "x", groupCovs = NULL, 
+#'
+#' plot1 <- plotGAM(gamFit = gam, smooth.cov = "x", groupCovs = NULL,
 #'                   rawOrFitted = "raw", plotCI=TRUE, orderedAsFactor = FALSE)
 #' gam <- mgcv::gam(y ~ s(x) + group + s(x, by=group), data=data)
-#' plot2 <- plotGAM(gamFit = gam, smooth.cov = "x", groupCovs = "group", 
+#' plot2 <- plotGAM(gamFit = gam, smooth.cov = "x", groupCovs = "group",
 #'                              rawOrFitted = "raw", orderedAsFactor = FALSE)
-
-
-
-
-
+#' @import ggplot2
+#' @importFrom stats update
 plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, rawOrFitted = F, plotCI = T) {
-  
+
   if (missing(gamFit)) { stop("gamFit is missing")}
   if (missing(smooth.cov)) { stop("smooth.cov is missing")}
-  
+
   if (class(smooth.cov) != "character") { stop("smooth.cov must be a character")}
   if (class(groupCovs) != "character" & (!is.null(groupCovs))) { stop("groupCovs must be a character")}
-  
-  
+
+
   fit <- fitted <- group <- se.fit <- NULL
-  
+
   if (base::class(gamFit)[1] != "gam") {
     stop("gamFit is not a object of type gam")
   }
-  
+
   base::print("Working with a GAM Model")
-  
+
   if (base::is.null(groupCovs)) {
-    
+
     gam <- gamFit
     temp.data <- gam$model
-    
+
     old.formula <- gam$formula
     old.covariates <- base::as.character(old.formula)[3]
     old.covariates <- base::gsub(" ", "", old.covariates)
     main.smooth <- base::paste0("s(", smooth.cov)
     interaction.smooth <- base::paste0("s(", smooth.cov, ",by=")
-    
+
     if (!base::grepl(pattern = main.smooth, old.covariates, fixed=T)) {
       base::stop("smooth.cov is not included in formula for original model fit")
     }
-    
+
     if (base::grepl(pattern = interaction.smooth, old.covariates, fixed=T)) {
       base::stop("smooth.cov has an interaction term in original model fit, pass groupCovs as an argument")
     }
-    
+
     plot.df = base::data.frame( x = base::seq(min(temp.data[smooth.cov]), max(temp.data[smooth.cov]), length.out=200))
     base::names(plot.df) <- smooth.cov
-    
+
     for (i in base::names(temp.data)[-1]) {
       if (i != smooth.cov) {
         if (base::any(base::class(temp.data[i][,1])[1] == c("numeric", "integer","boolean"))) {
@@ -82,8 +79,8 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
         }
       }
     }
-    
-        
+
+
     for (i in 1:dim(plot.df)[2]) {
       if (class(plot.df[,i])[1] == "ordered" |  class(plot.df[,i])[1] == "factor") {
         warning("There are one or more factors in the model fit, please consider plotting by group since plot might be unprecise")
@@ -92,9 +89,9 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
 
 
     plot.df = base::cbind(plot.df, base::as.data.frame(mgcv::predict.gam(gam, plot.df, se.fit = TRUE)))
-    
+
     if (plotCI) {
-      plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+      plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                + ggplot2::geom_line(ggplot2::aes(y=fit), size=1)
                + ggplot2::geom_ribbon(data=plot.df, ggplot2::aes(ymax = fit+1.96*se.fit, ymin = fit-1.96*se.fit, linetype=NA), alpha = .2)
                + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], "  vs. s(",smooth.cov,")"))
@@ -102,7 +99,7 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                + ggplot2::xlab(base::paste0("s(",smooth.cov,")"))
                + ggplot2::theme_bw())
     } else {
-      plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+      plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                + ggplot2::geom_line(ggplot2::aes(y=fit), size=1)
                + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], "  vs. s(",smooth.cov,")"))
                + ggplot2::ylab(base::paste0("Predicted ", base::as.character(old.formula)[2]))
@@ -110,7 +107,7 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                + ggplot2::theme_bw())
     }
 
-    
+
     if (rawOrFitted == "raw") {
       plot <- plot + ggplot2::geom_point(data = temp.data, ggplot2::aes(x=temp.data[smooth.cov], y=temp.data[,1]))
       base::return(plot)
@@ -121,31 +118,31 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
     } else {
       base::return(plot)
     }
-    
+
   } else if (base::is.character(groupCovs)) {
-    
+
     gam <- gamFit
     temp.data <- gam$model
-    
+
     old.formula <- gam$formula
     old.covariates <- base::as.character(old.formula)[3]
     old.covariates <- base::gsub(" ", "", old.covariates)
     main.smooth <- base::paste0("s(", smooth.cov)
     interaction.smooth <- base::paste0("s(", smooth.cov, ",by=")
-    
+
     if(regexpr(groupCovs, as.character(old.covariates))[1] == 1) {
       old.covariates <- paste0("+", old.covariates)
     }
-    
+
     main.group <- base::paste0("+", groupCovs)
     if (!base::grepl(pattern = main.group, old.covariates, fixed=T)) {
       base::stop("Error groupCovs is not included as a main effect in original model, include in original fit")
-    } 
-    
+    }
+
     temp.fm <- strsplit(old.covariates, split='+', fixed = T)[[1]]
     for (i in 1:length(temp.fm)) {
       if (grepl(main.smooth, temp.fm[i], fixed=T)) {
-        temp.term <- strsplit(temp.fm[i], split=',', fixed = T)[[1]] 
+        temp.term <- strsplit(temp.fm[i], split=',', fixed = T)[[1]]
         if (length(temp.term) > 1) {
           for (j in 2:length(temp.term)) {
             if (grepl('=',temp.term[j])) {
@@ -172,23 +169,23 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
         temp.fm[i] <- toString(temp.term)
       }
     }
-    
+
     old.covariates <- paste0(temp.fm, collapse="+")
     old.covariates <- base::gsub(" ", "", old.covariates)
-    
+
     if (!base::grepl(pattern = main.smooth, old.covariates, fixed=T) &  base::class(temp.data[groupCovs][,1])[1] == "ordered") {
       base::stop("smooth.cov is not included in formula for original model fit, model might be incorrectly parametrized")
     } else if (!base::grepl(pattern = interaction.smooth, old.covariates, fixed=T)) {
       base::warning("smooth.cov has no interaction term in original model fit")
-      
-      
+
+
       foo <-  base::seq(min(temp.data[smooth.cov]), max(temp.data[smooth.cov]), length.out=200)
       plot.df = base::data.frame( x = rep(foo, nlevels(as.factor(temp.data[groupCovs][,1]))))
       base::names(plot.df) <- smooth.cov
-      
+
       plot.df$temp <- rep(levels(as.factor(temp.data[groupCovs][,1])), each = 200)
       base::names(plot.df)[2] <- groupCovs
-      
+
       for (i in base::names(temp.data)[-1]) {
         if (!(i == smooth.cov | i == groupCovs)) {
           if (base::any(base::class(temp.data[i][,1])[1] == c("numeric", "integer","boolean"))) {
@@ -202,12 +199,12 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
           }
         }
       }
-      
+
       plot.df = base::cbind(plot.df, base::as.data.frame(mgcv::predict.gam(gam, plot.df, se.fit = TRUE)))
       plot.df$group <- plot.df[,2]
-      
+
       if (plotCI) {
-        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                  + ggplot2::geom_line(ggplot2::aes(y=fit, col=factor(group)), size=1)
                  + ggplot2::geom_ribbon(data=plot.df, ggplot2::aes(ymax = fit+1.96*se.fit, ymin = fit-1.96*se.fit, col=factor(group), linetype=NA), alpha = .2)
                  + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], " vs. s(",smooth.cov,")"))
@@ -215,9 +212,9 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                  + ggplot2::xlab(base::paste0("s(",smooth.cov,")"))
                  + ggplot2::scale_colour_discrete(name = groupCovs)
                  + ggplot2::theme_bw())
-        
+
       } else {
-        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                  + ggplot2::geom_line(ggplot2::aes(y=fit, col=factor(group)), size=1)
                  + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], " vs. s(",smooth.cov,")"))
                  + ggplot2::ylab(base::paste0("Predicted ", base::as.character(old.formula)[2]))
@@ -225,7 +222,7 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                  + ggplot2::scale_colour_discrete(name = groupCovs)
                  + ggplot2::theme_bw())
       }
-            
+
       if (rawOrFitted == "raw") {
         plot <- plot + ggplot2::geom_point(data = temp.data, ggplot2::aes(x=temp.data[smooth.cov], y=temp.data[,1], col=temp.data[groupCovs][,1]))
         base::return(plot)
@@ -236,20 +233,20 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
       } else {
         base::return(plot)
       }
-      
-    } 
-    
+
+    }
+
     else {
-      
+
       main.group <- base::paste0("+", groupCovs)
       if (!base::grepl(pattern = main.group, old.covariates, fixed=T)) {
         base::stop("groupCovs is not included as main effect in original model, please include in original fit")
       }
-      
+
       if (base::class(temp.data[groupCovs][,1])[1] == "ordered" & orderedAsFactor) {
         base::warning("groupCovs is an ordered variable in original fit, reffiting model with groupCovs as a factor")
         temp.data[groupCovs] <- as.factor(as.character(temp.data[groupCovs][,1]))
-        
+
         textCall <- gam$call
         temp.fm.updated <- strsplit(old.covariates, split='+', fixed = T)[[1]]
         index.Main <- setdiff(grep(pattern = main.smooth, x = temp.fm.updated, fixed = T),
@@ -259,25 +256,25 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
         updatedCovs <- base::gsub(" ", "", updatedCovs)
         updatedformula <- stats::as.formula(paste(old.formula[2], old.formula[1], updatedCovs))
         new.data <- temp.data
-        
+
         gam <- stats::update(gam, formula = updatedformula, data = new.data)
       }
-      
+
       temp.data <- gam$model
       old.formula <- gam$formula
       old.covariates <- base::as.character(old.formula)[3]
       old.covariates <- base::gsub(" ", "", old.covariates)
       main.smooth <- base::paste0("s(", smooth.cov, ")")
       interaction.smooth <- base::paste0("s(", smooth.cov, ",by=")
-      
-      
+
+
       foo <-  base::seq(min(temp.data[smooth.cov]), max(temp.data[smooth.cov]), length.out=200)
       plot.df = base::data.frame( x = rep(foo, nlevels(temp.data[groupCovs][,1])))
       base::names(plot.df) <- smooth.cov
-      
+
       plot.df$temp <- rep(levels(temp.data[groupCovs][,1]), each = 200)
       base::names(plot.df)[2] <- groupCovs
-      
+
       for (i in base::names(temp.data)[-1]) {
         if (!(i == smooth.cov | i == groupCovs)) {
           if (base::any(base::class(temp.data[i][,1])[1] == c("numeric", "integer","boolean"))) {
@@ -291,12 +288,12 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
           }
         }
       }
-      
+
       plot.df = base::cbind(plot.df, base::as.data.frame(mgcv::predict.gam(gam, plot.df, se.fit = TRUE)))
       plot.df$group <- plot.df[,2]
-      
+
       if (plotCI) {
-        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                  + ggplot2::geom_line(ggplot2::aes(y=fit, col=factor(group)), size=1)
                  + ggplot2::geom_ribbon(data=plot.df, ggplot2::aes(ymax = fit+1.96*se.fit, ymin = fit-1.96*se.fit, col=factor(group), linetype=NA), alpha = .2)
                  + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], "  vs. s(",smooth.cov,")"))
@@ -305,7 +302,7 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                  + ggplot2::scale_colour_discrete(name = groupCovs)
                  + ggplot2::theme_bw())
       } else {
-        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))   
+        plot <- (ggplot2::ggplot(data=plot.df, ggplot2::aes(x=plot.df[,1]))
                  + ggplot2::geom_line(ggplot2::aes(y=fit, col=factor(group)), size=1)
                  + ggplot2::ggtitle(base::paste0(base::as.character(old.formula)[2], "  vs. s(",smooth.cov,")"))
                  + ggplot2::ylab(base::paste0("Predicted ", base::as.character(old.formula)[2]))
@@ -314,7 +311,7 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
                  + ggplot2::theme_bw())
       }
 
-      
+
       if (rawOrFitted == "raw") {
         plot <- plot + ggplot2::geom_point(data = temp.data, ggplot2::aes(x=temp.data[smooth.cov], y=temp.data[,1], col=temp.data[groupCovs][,1]))
         base::return(plot)
@@ -326,11 +323,11 @@ plotGAM <- function(gamFit, smooth.cov , groupCovs = NULL, orderedAsFactor = T, 
         base::return(plot)
       }
     }
-    
+
   } else {
     stop("groupCovs is not a character or null please correct")
   }
-  
+
 }
 
 
--- R/vlmParam.R
+++ R/vlmParam.R
@@ -1,9 +1,9 @@
 #' Run a Linear Model on all voxels of a NIfTI image within a mask and and return parametric and smooth coefficients tables
-#'  
 #'
-#' This function is able to run a Linear Model using the stats package. 
-#' The analysis will run in all voxels in in the mask and will and will return parametric coefficients at each voxel. 
-#' 
+#'
+#' This function is able to run a Linear Model using the stats package.
+#' The analysis will run in all voxels in in the mask and will and will return parametric coefficients at each voxel.
+#'
 #' @param image Input image of type 'nifti' or vector of path(s) to images. If multiple paths, the script will all mergeNifti() and merge across time.
 #' @param mask Input mask of type 'nifti' or path to mask. Must be a binary mask
 #' @param fourdOut To be passed to mergeNifti, This is the path and file name without the suffix to save the fourd file. Default (NULL) means script won't write out 4D image.
@@ -12,9 +12,9 @@
 #' @param mc.preschedule Argument to be passed to mclapply, whether or not to preschedule the jobs. More info in parallel::mclapply
 #' @param ncores Number of cores to use
 #' @param ... Additional arguments passed to lm()
-#' 
+#'
 #' @return Return list of parametric and spline coefficients (include standard errors and p-values) fitted to each voxel over the masked images passed to function.
-#' 
+#'
 #' @export
 #' @examples
 #' image <- oro.nifti::nifti(img = array(1:1600, dim =c(4,4,4,25)))
@@ -23,63 +23,65 @@
 #' covs <- data.frame(x = runif(25), y = runif(25))
 #' fm1 <- "~ x + y"
 #' models <- vlmParam(image=image, mask=mask, formula=fm1, subjData=covs, ncores = 1)
-
+#' @importFrom oro.nifti readNIfTI
+#' @importFrom parallel mclapply
+#' @importFrom stats lm summary.lm
 
 vlmParam <- function(image, mask , fourdOut = NULL, formula, subjData, mc.preschedule = TRUE, ncores = 1, ...) {
-  
+
   if (missing(image)) { stop("image is missing")}
   if (missing(mask)) { stop("mask is missing")}
   if (missing(formula)) { stop("formula is missing")}
   if (missing(subjData)) { stop("subjData is missing")}
-  
+
   if (class(formula) != "character") { stop("formula class must be character")}
-  
-  
+
+
   if (class(image) == "character" & length(image) == 1) {
     image <- oro.nifti::readNIfTI(fname=image)
   } else if (class(image) == "character" & length(image) > 1) {
     image <- mergeNiftis(inputPaths = image, direction = "t", outfile <- fourdOut)
   }
-  
+
   if (class(mask) == "character" & length(mask) == 1) {
     mask <- oro.nifti::readNIfTI(fname=mask)
   }
-  
-  
+
+
   imageMat <- ts2matrix(image, mask)
-  
+
   voxNames <- names(imageMat)
-  
-  
+
+
   rm(image)
   rm(mask)
   gc()
-  
+
   print("Created time series to matrix")
-  
-  m <- parallel::mclapply(voxNames, 
+
+  m <- parallel::mclapply(voxNames,
                           FUN = listFormula, formula, mc.cores = ncores)
-  
-  imageMat <- cbind(imageMat, subjData) 
-  
+
+  imageMat <- cbind(imageMat, subjData)
+
   print("Created formula list")
   timeIn <- proc.time()
-  
+
   print("Running test model")
   model <- stats::lm(m[[1]], data=imageMat, ...)
-  
-  
+
+
   print("Running parallel models")
-  model <- parallel::mclapply(m, 
+  model <- parallel::mclapply(m,
                               FUN = function(x, data, ...) {
                                 foo <- stats::summary.lm(stats::lm(x, data=data, ...))
                                 return(foo$coefficients)
                               }, data=imageMat, ..., mc.preschedule = mc.preschedule, mc.cores = ncores)
-  
+
   timeOut <- proc.time() - timeIn
   print(timeOut[3])
   print("Parallel Models Ran")
-  
+
   return(model)
-  
+
 }
